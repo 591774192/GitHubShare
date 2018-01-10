7a4518e2007056858a8d92882edca8ebaf0fa7cd
591774192@qq.com <guobao0730>
2018-01-10 17:57:34 +0800

    测试编辑再次提交

---
+++ app/src/main/java/com/gjcw/githubshare/TestOne.java
@@ -0,0 +1,22 @@
+package com.gjcw.githubshare;
+
+/**
+ * @author 郭宝
+ * @project： GitHubShare
+ * @package： com.gjcw.githubshare
+ * @date： 2018/1/10/010 17:56
+ * @brief:
+ */
+
+public class TestOne {
+
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
